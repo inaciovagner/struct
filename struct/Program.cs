c35a2606496601f58b4f0d1a5265265ce7a5721d
Vagner de Souza <inaciovagner1@gmail.com>
2021-03-30 12:44:09 +0100

    Add project files.

---
+++ struct/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @struct
+{
+    class Program
+    {
+       struct Student
+        {
+            public string Name;
+            public string Grade;
+
+            public class structure
+            {
+                static void Main(string[] args)
+                {
+                    Student st1;
+                    st1.Name = Console.ReadLine();
+                    st1.Grade = Console.ReadLine();
+                    Console.WriteLine(st1.Name + " is from " + st1.Grade);
+                }
+            }
+        }
+    }
+}
